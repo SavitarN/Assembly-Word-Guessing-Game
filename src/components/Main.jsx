048c24afb51242f99ad47c97e9f10de6c0b10021
SavitarN <nischalzoc999@gmail.com>
2025-01-27 21:55:16 +0545

    color-chips added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import languages from "../languages";
+import "./style.css";
 const Main = () => {
-  return <div></div>;
+  const chipsElem = languages.map((elem) => {
+    return (
+      <span
+        style={{ backgroundColor: elem.backgroundColor, color: elem.color }}
+      >
+        {elem.name}
+      </span>
+    );
+  });
+
+  return (
+    <main>
+      <div className="container">{chipsElem}</div>
+    </main>
+  );
 };
 
 export default Main;
