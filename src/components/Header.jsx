a00d2cc2a5ee3d64288f7e9f9838cdc50ee658b4
SavitarN <nischalzoc999@gmail.com>
2025-01-27 16:26:09 +0545

    header-section

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css";
+const Header = () => {
+  return (
+    <header>
+      <h2>Assembly:Endgame</h2>
+      <p>
+        Guess the word in under 8 attempts to keep the programming world safe
+        from Assembly!
+      </p>
+    </header>
+  );
+};
+
+export default Header;
