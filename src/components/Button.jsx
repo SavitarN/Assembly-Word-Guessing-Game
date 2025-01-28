04b3dc077762f94067328efc9b16078d2fe70ca6
SavitarN <nischalzoc999@gmail.com>
2025-01-28 15:55:53 +0545

    keyword

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+const Button = (props) => {
+  return (
+    <>
+      <button>{props.char}</button>
+    </>
+  );
+};
+
+export default Button;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 import languages from "../languages";
+import Button from "./Button";
 import "./style.css";
+
 const Main = () => {
+  const [currentWord, setCurrentWord] = React.useState("react");
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  const alphabetButton = alphabet
+    .split("")
+    .map((char) => <Button char={char.toUpperCase()} />);
+
+  const wordElem = currentWord
+    .split("")
+    .map((elem) => <span>{elem.toUpperCase()}</span>);
   const chipsElem = languages.map((elem) => {
     return (
       <span
-        style={{ backgroundColor: elem.backgroundColor, color: elem.color }}
+        style={{
+          backgroundColor: elem.backgroundColor,
+          color: elem.color,
+          borderRadius: "5px",
+          padding: "5px",
+        }}
       >
         {elem.name}
       </span>
@@ -13,9 +29,13 @@ const Main = () => {
   });
 
   return (
-    <main>
+    <div className="main">
       <div className="container">{chipsElem}</div>
-    </main>
+
+      <div className="words">{wordElem}</div>
+
+      <div className="alphabets">{alphabetButton}</div>
+    </div>
   );
 };
 
